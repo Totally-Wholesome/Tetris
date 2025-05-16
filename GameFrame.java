098faaff3891ffc8535f71e952867f5e02c9a6cf
Totally-Wholesome <oliver.tung168@gmail.com>
2025-05-15 21:38:10 -0600

    Update GameFrame.java

--- GameFrame.java
+++ GameFrame.java
@@ -1,15 +1,13 @@
 import javax.swing.*;
 
 public class GameFrame extends JFrame {
-	private GamePanel gamePanel = new GamePanel();
-    
     public GameFrame() {
+    	this.add(new GamePanel());
     	this.setTitle("Java Tetris");
         this.setSize(315, 635);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setResizable(false);
         this.setLocationRelativeTo(null);
-        this.add(gamePanel);
         this.setVisible(true);
     }
 }
